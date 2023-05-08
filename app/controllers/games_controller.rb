9c923648c17623cc558f9ea23d9b5a49ecc14571
joe weber <jweber4213@gmail.com>
2023-05-08 16:43:25 +0900

    time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,19 +3,31 @@ require 'json'
 
 class GamesController < ApplicationController
   def new
-    @letters = ('a'..'z').to_a.sample(10)
+    @load_time = Time.now
+    vowels = %w[a e i o u]
+    @letters = []
+    2.times do
+      @letters << vowels.sample
+    end
+    # while @letters.count < 10
+    #   @letters << ('a'..'z').to_a.sample
+    # end
+    @letters += ('a'..'z').to_a.sample(8)
+    @letters.shuffle
   end
 
   def score
     grid = params[:letters].split
     attempt = params[:guess]
+    load_time = Time.parse(params[:load_time])
+    post_time = Time.now
+    r_s = 1 / (post_time - load_time) * attempt.length
     word_hash = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{attempt.downcase}").read)
-    print word_hash
     if attempt.chars.all? { |c| grid.include?(c) } && get_bool(attempt, grid)
       if word_hash.key?("error")
-        @result = "#{attempt} is not an english word. "
+        @result = "score: 0 & #{attempt} is not an english word. "
       else
-        @result = "Well done, #{attempt} is a word & in the grid."
+        @result = "score: #{(r_s*10).round(2)} Well done, #{attempt} is a word & in the grid."
       end
     else
       @result = "#{attempt} is not in the grid. "
